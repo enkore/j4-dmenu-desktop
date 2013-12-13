13ac89d6e33a7fa0b75d64abb6ab69ab9578f71c
enkore <public@enkore.de>
2013-12-13 10:33:48 +0100

    TestFileFinder

---
+++ src/TestFileFinder.cc
@@ -0,0 +1,32 @@
+#include "FileFinder.hh"
+#include "catch.hpp"
+
+static const std::string test_files = TEST_FILES;
+
+std::vector<std::string> ff2l(FileFinder &ff)
+{
+    std::list<std::string> l;
+    while (ff++)
+	l.push_back(*ff);
+    l.sort(); // Deterministic ordering
+    return std::vector<std::string>(l.begin(), l.end());;
+}
+
+TEST_CASE("FileFinder/find", "")
+{
+    FileFinder ff(TEST_FILES, ".desktop");
+    auto files = ff2l(ff);
+    REQUIRE( files.size() == 4 );
+    REQUIRE( files[0] == test_files + "applications/eagle.desktop" );
+    REQUIRE( files[1] == test_files + "applications/gimp.desktop" );
+    REQUIRE( files[2] == test_files + "applications/htop.desktop" );
+    REQUIRE( files[3] == test_files + "file.desktop" );
+}
+
+TEST_CASE("FileFinder/find2", "")
+{
+    FileFinder ff(TEST_FILES, ".ext");
+    auto files = ff2l(ff);
+    REQUIRE( files.size() == 1 );
+    REQUIRE( files[0] == test_files + "other.ext" );
+}
