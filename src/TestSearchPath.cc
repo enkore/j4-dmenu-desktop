4652a2a7b5632fe49fe1eaad85b3288b6914894d
enkore <public@enkore.de>
2013-12-13 11:47:43 +0100

    TestSearchPath: Make tests machine independent

--- src/TestSearchPath.cc
+++ src/TestSearchPath.cc
@@ -4,6 +4,8 @@
 #include "SearchPath.hh"
 #include "catch.hpp"
 
+static const std::string test_files = TEST_FILES;
+
 TEST_CASE("SearchPath/noenv", "Check SearchPath default values")
 {
     unsetenv("XDG_DATA_HOME");
@@ -38,12 +40,12 @@ TEST_CASE("SearchPath/XDG_DATA_DIRS", "Check SearchPath honors XDG_DATA_DIRS")
 {
     unsetenv("XDG_DATA_HOME");
     setenv("HOME", "/home/testuser", 1);
-    setenv("XDG_DATA_DIRS", "/usr/share/:/usr/local/share/", 1);
+    setenv("XDG_DATA_DIRS", (test_files + "usr/share/:" + test_files + "usr/local/share/").c_str(), 1);
 
     SearchPath sp;
     std::vector<std::string> result(sp.begin(), sp.end());
     
     REQUIRE( result.size() == 2 );
-    REQUIRE( result[0] == "/usr/share/applications/" );
-    REQUIRE( result[1] == "/usr/local/share/applications/" );
+    REQUIRE( result[0] == test_files + "usr/share/applications/" );
+    REQUIRE( result[1] == test_files + "usr/local/share/applications/" );
 }
