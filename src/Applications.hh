9c95935da9e77059a50abef07b811d38ba3045a5
enkore <public@enkore.de>
2015-10-21 17:40:33 +0200

    Cleanup Applications::update_log

--- src/Applications.hh
+++ src/Applications.hh
@@ -81,7 +81,7 @@ public:
         FILE *fp = fopen(write_file.str().c_str(), "w");
         if(!fp) {
             fprintf(stderr, "Can't write usage log '%s': %s\n", log, strerror(errno));
-            goto exit;
+            return;
         }
 
         app->usage_count++;
@@ -91,20 +91,16 @@ public:
                 continue;
             if(fprintf(fp, "%u,%s\n", app.second->usage_count, app.first.c_str()) < 0) {
                 perror("Write error");
-                goto exit;
+                fclose(fp);
+                return;
             }
         }
 
         fclose(fp);
-        fp = 0;
 
         if(rename(write_file.str().c_str(), log)) {
             perror("rename failed");
         }
-
-    exit:
-        if(fp)
-            fclose(fp);
     }
 };
 
