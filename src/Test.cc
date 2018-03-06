0f449cdd05626dd874eebb1fb39d6a24bef32b44
Marian Beermann <public@enkore.de>
2018-03-06 11:10:05 +0100

    don't create a dozen CUs for tests, bundle it all into one.

--- src/Test.cc
+++ src/Test.cc
@@ -3,3 +3,12 @@
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+static const std::string test_files = TEST_FILES;
+
+#include "TestApplication.cc"
+#include "TestApplicationRunner.cc"
+#include "TestSearchPath.cc"
+#include "TestLocaleSuffixes.cc"
+#include "TestFileFinder.cc"
+#include "TestFormatters.cc"
