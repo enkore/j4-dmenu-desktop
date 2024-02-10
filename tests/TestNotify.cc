12685a60cda7afc727785724a4bde8a5fc317755
meator <meator.dev@gmail.com>
2024-02-10 17:20:47 +0100

    TestNotify: Rename FILENAME to TEST_FILENAME
    FILENAME clashes with FILENAME enum member in HistoryManager::is_v0().
    This gets triggered in unity builds.

--- tests/TestNotify.cc
+++ tests/TestNotify.cc
@@ -9,7 +9,7 @@
 #include "NotifyInotify.hh"
 #endif
 
-#define FILENAME TEST_FILES "usr/local/share/newfile"
+#define TEST_FILENAME TEST_FILES "usr/local/share/newfile"
 
 TEST_CASE("Test detection of file creation and deletion of a subdirectory of "
           "watched directory",
@@ -21,27 +21,27 @@ TEST_CASE("Test detection of file creation and deletion of a subdirectory of "
     NotifyInotify notify(search_path);
 #endif
 
-    // Try to delete FILENAME if it exists. A failed test might not have
+    // Try to delete TEST_FILENAME if it exists. A failed test might not have
     // properly deleted it, so this assures that it won't interfere with tests.
-    if (unlink(FILENAME) == -1 && errno != ENOENT)
-        FAIL("Couldn't remove " FILENAME ": " << strerror(errno));
+    if (unlink(TEST_FILENAME) == -1 && errno != ENOENT)
+        FAIL("Couldn't remove " TEST_FILENAME ": " << strerror(errno));
 
     pollfd towait = {notify.getfd(), POLLIN, 0};
 
-    FILE *file = fopen(FILENAME, "w");
+    FILE *file = fopen(TEST_FILENAME, "w");
     if (!file)
-        FAIL("Couldn't create " FILENAME ": " << strerror(errno));
+        FAIL("Couldn't create " TEST_FILENAME ": " << strerror(errno));
     fprintf(file, "DATA");
     fclose(file);
 
     // Timeout of 65 seconds is set because NotifyKqueue polls events in one
     // minute intervals. NotifyInotify should return immediately on these polls.
     if (poll(&towait, 1, 65000) != 1)
-        FAIL("Notify didn't detect the creation of " FILENAME);
+        FAIL("Notify didn't detect the creation of " TEST_FILENAME);
     bool found = false;
     for (const auto &i : notify.getchanges()) {
-        // test if the newly created FILENAME is detected
-        if (i.name == &FILENAME[strlen(TEST_FILES "usr/")]) {
+        // test if the newly created TEST_FILENAME is detected
+        if (i.name == &TEST_FILENAME[strlen(TEST_FILES "usr/")]) {
             found = true;
             REQUIRE(i.rank == 0);
             REQUIRE(i.status == NotifyBase::modified);
@@ -54,14 +54,14 @@ TEST_CASE("Test detection of file creation and deletion of a subdirectory of "
     // therefore notify.getfd() shouldn't be readable
     REQUIRE(poll(&towait, 1, 0) == 0);
 
-    if (unlink(FILENAME) == -1)
-        FAIL("Couldn't remove " FILENAME ": " << strerror(errno));
+    if (unlink(TEST_FILENAME) == -1)
+        FAIL("Couldn't remove " TEST_FILENAME ": " << strerror(errno));
 
     if (poll(&towait, 1, 65000) != 1)
-        FAIL("Notify didn't detect the deletion of " FILENAME);
+        FAIL("Notify didn't detect the deletion of " TEST_FILENAME);
     found = false;
     for (const auto &i : notify.getchanges()) {
-        if (i.name == &FILENAME[strlen(TEST_FILES "usr/")]) {
+        if (i.name == &TEST_FILENAME[strlen(TEST_FILES "usr/")]) {
             found = true;
             REQUIRE(i.rank == 0);
             REQUIRE(i.status == NotifyBase::deleted);
